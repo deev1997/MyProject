81dd29ae6718a90bbeac11f34231b9875973cb44
deev1997 <tmm.mega@gmail.com>
2025-03-06 16:03:58 +0000

    Add files via upload

---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "vm_public_ip" {
+  description = "The public IP address of the VM"
+  value       = azurerm_public_ip.vm_public_ip.ip_address
+}
+
+output "vm_id" {
+  description = "The ID of the VM"
+  value       = azurerm_virtual_machine.main.id
+}
+
+output "vm_name" {
+  description = "The name of the VM"
+  value       = azurerm_virtual_machine.main.name
+}
